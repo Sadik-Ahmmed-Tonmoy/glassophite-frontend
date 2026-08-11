359bfd732abc8f26deec67700a4d03260d996a7b
Sadik Ahmmed Tonmoy <Sadikahmed2258@gmail.com>
2026-08-12 02:36:25 +0600

    update

--- src/components/pages/home/HomeComponent.tsx
+++ src/components/pages/home/HomeComponent.tsx
@@ -2,14 +2,13 @@
 
 import { FloatButton } from "@/components/ui/FloatButton/FloatButton";
 import BestSellerCarouselSection from "./BestSellerCarouselSection";
-import GlassophitePromiseSection from "./GlassophitePromiseSection";
 import HeroCinematicSection from "./HeroCinematicSection";
+import MoreProductsShowcaseSection from "./MoreProductsShowcaseSection";
 import NewArrivalsSection from "./NewArrivalsSection";
 import SEOContentSection from "./SEOContentSection";
 import ScrollAnimationEffect from "./ScrollAnimationEffect";
 import Testimonials from "./TestimonialsSection";
 import WhyChooseGlassophiteSection from "./WhyChooseGlassophiteSection";
-import MoreProductsShowcaseSection from "./MoreProductsShowcaseSection";
 
 const HomeComponent = () => {
   return (
@@ -23,7 +22,7 @@ const HomeComponent = () => {
       {/* <StyleLookbookShowcaseSection /> */}
       <ScrollAnimationEffect />
       <Testimonials />
-      <GlassophitePromiseSection />
+      {/* <GlassophitePromiseSection /> */}
       <SEOContentSection />
       <div className="hidden md:block">
         <FloatButton.BackTop />
